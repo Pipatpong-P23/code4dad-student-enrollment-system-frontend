a03aa44e5bbaca54613a577b9e3bb08cba667ec8
PpoH <pipatpong987123@gmail.com>
2024-02-18 17:20:43 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1> HELLO POOH </h1>
+        <h1> HELLO POOH2 </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

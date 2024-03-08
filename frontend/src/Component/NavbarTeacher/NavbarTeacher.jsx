73a2c2d9e6e24f11cb594664f1926d1ab98bad6d
PpoH <pipatpong987123@gmail.com>
2024-03-08 16:24:31 +0700

    add function navbar teacher

--- frontend/src/Component/NavbarTeacher/NavbarTeacher.jsx
+++ frontend/src/Component/NavbarTeacher/NavbarTeacher.jsx
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./NavbarTeacher.css";
 import { FiMenu } from "react-icons/fi";
 import { GiSpellBook } from "react-icons/gi";
-import HomePageTeacher from '../../HomePageTeacher/HomePageTeacher';
+import axios from 'axios';
 
 function NavbarTeacher({teacher_id}) {
     const [clicked, setClicked] = useState(false);
+    const [teacher_name , setTeacherName] = useState('');
+
+    useEffect(() => {
+        async function get_teacher_data() {
+            try{
+                const response = await axios.get(`http://oop.okusann.online:8088/get_teacher_by_teacher_id/${teacher_id}`);
+        
+                if (response.status == 200) {
+                    console.log("OK", response.data.name);
+                    setTeacherName(response.data.name);
+                    return ;
+                }
+
+            }catch (error) {
+                console.error(error);
+                return 
+        }
+    }
+    get_teacher_data();
+    console.log(teacher_name);
+    }, [teacher_id]);
 
     function logout(){
         document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
@@ -38,7 +59,7 @@ function NavbarTeacher({teacher_id}) {
                     <li> <a href='/view student'>View Student</a></li>
                 </ul>
                 <ul id='navbar'>
-                    <li> <a href=''>{teacher_id}</a> </li>
+                    <li> <a href=''>{teacher_name}</a> </li>
                     <li> <a onClick={logout}>Logout</a> </li>
                 </ul>
             </div>
--- frontend/src/HomePageTeacher/HomePageTeacher.jsx
+++ frontend/src/HomePageTeacher/HomePageTeacher.jsx
@@ -1,13 +1,56 @@
 import React , {useState , useEffect} from 'react'
 import NavbarTeacher from '../Component/NavbarTeacher/NavbarTeacher'
 import { getUsername } from '../Authentication'
+import axios from 'axios'
+import DataTable from 'react-data-table-component'
+import DropdownDate from '../Component/DropdownDateOption/DropdownDate'
+import './HomePageTeacher.css'
 
 function HomePageTeacher() {
     const teacher_name = getUsername();
+    const [selectedDate, setSelectedDate] = useState({ semester: '1', year: '2024' });
+    const [isview, setIsview] = useState(false);
+
+    const handleDateChange = (semester, year) => {
+        setSelectedDate({ semester, year });
+    };
+
+    const view_section = () => {
+        setIsview(!isview);
+    }
     
+    useEffect(() => {
+        async function get_all_section() {
+            try{
+                const response = await axios.get(`http://oop.okusann.online:8088/get_all_sections_taught_by_teacher_id/${teacher_name}/${selectedDate.semester}/${selectedDate.year}`);
+                if (response.status == 200) {
+                    console.log("OK", response.data);
+                    if (response.data.length > 0) {
+                        console.log(response.data);
+                    }
+                    else if (response.data.length == 0){
+                        alert("No data found");
+                    }
+                }
+                else{
+                    alert("Something went wrong");
+                }
+                }catch (error) {
+                    alert("No data found");
+                    return 
+        }
+    }
+    get_all_section();
+    }, [isview]);
+
     return (
         <div>
             <NavbarTeacher teacher_id={teacher_name} />
+            <div className='dropdowndate'>
+                <DropdownDate  onDateChange={handleDateChange}/>
+                <button onClick={view_section}> view </button>
+            </div>
+            
         </div>
     )
 }

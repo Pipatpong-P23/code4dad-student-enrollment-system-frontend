88488cd0ed48f43d1574c3bd4e9d659acfd55f1f
PrompipatJa <prompipat18348@gmail.com>
2024-03-10 05:07:32 +0700

    ปรับcss หน้า admin

--- frontend/src/HomePageAdmin/HomePageAdmin.jsx
+++ frontend/src/HomePageAdmin/HomePageAdmin.jsx
@@ -4,20 +4,26 @@ import './HomePageAdmin.css'
 
 function HomePageAdmin() {
   return (
-    <div>
-        <NavbarAdmin admin_id='Admin'/>
-        <h1 id='textadmin'>Admin</h1>
-        
-        <div>
-          <div>
-            <button className='buttonadmin' onClick={() => { window.location.href = '/add user'}}>Add User</button>
+    <div className='bgAd'>
+      <NavbarAdmin admin_id='Admin'/>
+        <div className="Adcontainer">
+
+          <div className='Adtopic'>
+            <h1 className='Adtext'>Admin</h1>
           </div>
-          <div>
-            <button className='buttonadmin' onClick={() => { window.location.href = '/add course'}}>Create Course</button>
-        </div>
-        <div>
-          <button className='buttonadmin' onClick={() => { window.location.href = '/add section'}}>Add Teacher in Section</button>
-        </div>
+        
+          <div className='Adbtn'>
+            <div>
+              <button className='Adbtninteract-AU' onClick={() => { window.location.href = '/add user'}}>Add User</button>
+            </div>
+            <div>
+              <button className='Adbtninteract-CC' onClick={() => { window.location.href = '/add course'}}>Create Course</button>
+            </div>
+            <div>
+              <button className='Adbtninteract-ATS' onClick={() => { window.location.href = '/add section'}}>Add Teacher in Section</button>
+            </div>
+          </div>
+
         </div>
     </div>
   )

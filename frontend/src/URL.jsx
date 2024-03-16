d86c8753089f1f011d23cb877b74b1e1d02bd654
PrompipatJa <prompipat18348@gmail.com>
2024-03-16 16:20:02 +0700

    Update URL.jsx

--- frontend/src/URL.jsx
+++ frontend/src/URL.jsx
@@ -1,2 +1,2 @@
-// export const url = 'http://oop.okusann.online:8088';
-export const url = 'http://localhost:8088';
+export const url = 'http://oop.okusann.online:8088';
+// export const url = 'http://localhost:8088';
